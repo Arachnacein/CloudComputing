5a920196ff1c15620a7ba7e8132daab36545064c
Jakub <jakub26800@gmail.com>
2023-11-13 18:04:31 +0100

    Fill project structure and first logic code.

--- CloudComputing/FormDesign/Form1.Designer.cs
+++ CloudComputing/FormDesign/Form1.Designer.cs
@@ -29,20 +29,78 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridView1 = new DataGridView();
+            xDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            yDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            radiusDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            isActiveDataGridViewCheckBoxColumn = new DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { xDataGridViewTextBoxColumn, yDataGridViewTextBoxColumn, radiusDataGridViewTextBoxColumn, isActiveDataGridViewCheckBoxColumn });
+            dataGridView1.Location = new Point(293, 147);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(674, 279);
+            dataGridView1.TabIndex = 0;
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            xDataGridViewTextBoxColumn.HeaderText = "X";
+            xDataGridViewTextBoxColumn.MinimumWidth = 8;
+            xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            xDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            yDataGridViewTextBoxColumn.HeaderText = "Y";
+            yDataGridViewTextBoxColumn.MinimumWidth = 8;
+            yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            yDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // radiusDataGridViewTextBoxColumn
+            // 
+            radiusDataGridViewTextBoxColumn.DataPropertyName = "Radius";
+            radiusDataGridViewTextBoxColumn.HeaderText = "Radius";
+            radiusDataGridViewTextBoxColumn.MinimumWidth = 8;
+            radiusDataGridViewTextBoxColumn.Name = "radiusDataGridViewTextBoxColumn";
+            radiusDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // isActiveDataGridViewCheckBoxColumn
+            // 
+            isActiveDataGridViewCheckBoxColumn.DataPropertyName = "IsActive";
+            isActiveDataGridViewCheckBoxColumn.HeaderText = "IsActive";
+            isActiveDataGridViewCheckBoxColumn.MinimumWidth = 8;
+            isActiveDataGridViewCheckBoxColumn.Name = "isActiveDataGridViewCheckBoxColumn";
+            isActiveDataGridViewCheckBoxColumn.Width = 150;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1418, 844);
+            Controls.Add(dataGridView1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Cloud Computing - J.Łuka, Ł.Grochowski, M.Bielicki";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn radiusDataGridViewTextBoxColumn;
+        private DataGridViewCheckBoxColumn isActiveDataGridViewCheckBoxColumn;
     }
 }
--- CloudComputing/FormDesign/Form1.cs
+++ CloudComputing/FormDesign/Form1.cs
@@ -1,10 +1,23 @@
+using CloudComputing.Services;
+
 namespace CloudComputing
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly IFilesReaderService _readerService;
+
+        public Form1(IFilesReaderService filesReader)
         {
             InitializeComponent();
+            _readerService = filesReader;
+
+            Start();
+        }
+
+        private void Start()
+        {
+            List<Models.Point> points = _readerService.ReadData("dane1.txt");
+            dataGridView1.DataSource = points;
         }
     }
 }
