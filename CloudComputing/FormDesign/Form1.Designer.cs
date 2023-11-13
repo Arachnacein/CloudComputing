86f4597369639c008a0f115cbdb340a4bb9cd5ed
Jakub <jakub26800@gmail.com>
2023-11-13 15:59:38 +0100

    Create folder, setup icons.

--- CloudComputing/FormDesign/Form1.Designer.cs
+++ CloudComputing/FormDesign/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1418, 844);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cloud Computing - J.Łuka, Ł.Grochowski, M.Bielicki";
+            ResumeLayout(false);
         }
 
         #endregion
